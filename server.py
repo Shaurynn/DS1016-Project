ea8ea952d2fbb45403289f17309290c0cad3c244
Kenneth Tan <great.barrier.keef@gmail.com>
2022-11-30 17:35:47 +0800

    test

--- server.py
+++ server.py
@@ -3,20 +3,21 @@ from fastapi import UploadFile, File
 import uvicorn
 import shutil
 import streamlit as st
-
-import shutil
 import tensorflow as tf
 import os
 import gzip
 import tarfile
-
 import pandas as pd
 import matplotlib.pyplot as plt
 from collections import Counter
 from tensorflow.keras.callbacks import EarlyStopping
 
-from prediction import preprocess, model_loading, predict
+from prediction import preprocess, predict
 
+st.markdown("""
+# Welcome to Atulya's Project
+## "How to give my team-mates aneurysms"
+""")
 
 app = FastAPI()
 
@@ -24,8 +25,8 @@ app = FastAPI()
 def ItsWorking():
     return "It's Working!"
 
-@app.post('/api/predict')
-def predict_image(file: UploadFile = File(...), ref_atlas: UploadFile = File(...)):
+st.cache
+def predict_image(file: , ref_atlas: UploadFile = File(...)):
     with open("./input.nii", "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
     with open("./ref_atlas.nii", "wb") as buffer:
@@ -36,6 +37,36 @@ def predict_image(file: UploadFile = File(...), ref_atlas: UploadFile = File(...
     prediction = predict()
 
     return {'prediction':1}
+
+uploaded_file = st.file_uploader("Please uploade your MRI nifti file")
+
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
+
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    st.write(stringio)
+
+    # To read file as string:
+    string_data = stringio.read()
+    st.write(string_data)
+
+uploaded_atlas = st.file_uploader("Please upload your atlas nifti file")
+
+if uploaded_atlas is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
+
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    st.write(stringio)
+
+    # To read file as string:
+    string_data = stringio.read()
+    st.write(string_data)
 
 
 if __name__ == "__main__":
