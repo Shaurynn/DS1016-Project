da09995e3b9543c544dbe06ce2320244676f5c94
Kenneth Tan <great.barrier.keef@gmail.com>
2022-12-01 12:22:48 +0800

    test

--- server.py
+++ server.py
@@ -39,6 +39,6 @@ def detect_AD():
     preprocess(os.path.join("./",image.name),os.path.join("./", atlas.name))
     #saved_model = os.path.join("./",model)
     prediction = predict(f"./{image.name}_2d.npy")
-    st.success(prediction)
+    st.text(prediction)
 
 trigger = st.button('Predict', on_click=detect_AD)
